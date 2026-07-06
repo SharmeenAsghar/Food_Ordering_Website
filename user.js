f5b3415fdc6ace89cd83828b9c9205ef340b549e
SharmeenAsghar <sharmeenasghar78@gmail.com>
2026-07-06 04:19:00 -0700

    Backend- models/
    connect server with mongodb

---
+++ user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 8
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
